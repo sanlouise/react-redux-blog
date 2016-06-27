391f8b688358cf96fee6cd4c192305a5a665f013
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-27 22:07:30 +0200

    Set up API request to reduxblog

--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,8 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts_index';
 
-const Greeting = () => {
-	return <div>Hello!</div>;
-};
-
 export default (
 	<Route path="/" component={App}>
 		<IndexRoute component={PostsIndex} />
-		<Route path="greet" component={Greeting} />
-		<Route path="greet2" component={Greeting} />
-		<Route path="greet3" component={Greeting} />
 	</Route>
 );
