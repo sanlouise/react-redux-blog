38ea92faf0a75679bc6f949eb64920520055e8dd
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-06-27 21:54:24 +0200

    Create index route and child routes

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
+//Ensure that children in nested routes are also rendering.
+//This.props.children are defined in routes
 export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
-  }
+	render() {
+		return (
+			<div>
+			{this.props.children}
+			</div>
+		);
+	}
 }
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
+import PostsIndex from './components/posts_index';
+
+const Greeting = () => {
+	return <div>Hello!</div>;
+};
 
 export default (
- <Route path="/" component={App} />
+	<Route path="/" component={App}>
+		<IndexRoute component={PostsIndex} />
+		<Route path="greet" component={Greeting} />
+		<Route path="greet2" component={Greeting} />
+		<Route path="greet3" component={Greeting} />
+	</Route>
 );
