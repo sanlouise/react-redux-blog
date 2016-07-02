2f69f06aa82ebf885f5a6a7eb4aabbcc4f02b5dd
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-07-02 23:24:54 +0200

    Update posts_reducer

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,12 +1,13 @@
 import { FETCH_POSTS } from '../actions/index';
 
+
 //Post is show action, [] list of blog posts
 const INITIAL_STATE = { all: [], post: null};
 
 export default function(state = INITIAL_STATE, action) {
 	switch(action.type) {
 		case FETCH_POSTS:
-
+			return { ...state, all: action.payload.data };
 		default:
 			return state;
 	}
