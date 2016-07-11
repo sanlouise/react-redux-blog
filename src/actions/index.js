1f1fc99137f3785c8e236afe5434740d9ad07ba3
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-07-11 22:07:16 +0200

    Minor fixes

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ export const FETCH_POSTS = 'FETCH_POSTS';
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=34567809765476';
 
-export fetchPosts () {
+export function fetchPosts (id) {
 	const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 	return {
 		type: FETCH_POSTS,
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,6 +1,5 @@
 import { FETCH_POSTS } from '../actions/index';
 
-
 //Post is show action, [] list of blog posts
 const INITIAL_STATE = { all: [], post: null};
 
