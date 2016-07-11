03838836d401df567bd6b2c1d945797cc7fbd452
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-07-11 22:26:58 +0200

    Fix bug importing reducer

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 class PostsIndex extends Component {
+	componentWillMount() {
+		console.log('Should call action creator FETCH_POSTS')
+	}
 	render() {
 		return (
 			<div>All blog posts</div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,7 @@ import { combineReducers } from 'redux';
 import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
-  state: (state = {
-  	posts: PostsReducer
+  posts: PostsReducer
 });
 
 export default rootReducer;
