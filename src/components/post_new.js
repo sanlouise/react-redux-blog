7d38ee8b8179037ba110c308560646621da13204
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-07-12 15:20:29 +0200

    Install and wire up Redux Form for new blog posts

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 
 class PostNew extends Component {
 	render() {
 		return (
-			<div>Create Form </div>
+			<form>
+				<h4>Create a post</h4>
+				<div className="form-group">
+					<label>Title</label>
+					<input type="text" className="form-control" />
+				</div>
+
+				<div className="form-group">
+					<label>Categories</label>
+					<input type="text" className="form-control" />
+				</div>
+
+				<div className="form-group">
+					<label>Body</label>
+					<textarea className="form-control" />
+				</div>
+			</form>
 		);
 	}
 }
 
-export default PostNew;
+export default reduxForm({
+	form: 'PostNewForm',
+	fields: ['title', 'categories', 'content']
+})(PostNew);
