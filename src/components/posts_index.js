a132b649b09ad6a210a2c9f4fc7fb025602c3775
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-07-12 11:11:36 +0200

    Add link to blog form

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from '../actions/index'
+import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
 export default class PostsIndex extends Component {
 
@@ -10,7 +11,15 @@ export default class PostsIndex extends Component {
 	}
 	render() {
 		return (
-			<div>All blog posts</div>
+			<div>
+				<div className="text-xs-right">
+					<Link to="/posts/new" className="btn btn-primary">
+						Create Posts
+					</Link>
+				</div>
+				All blog posts
+			</div>
+
 		);
 	}
 }
