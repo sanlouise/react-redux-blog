f479b75dc0ea70103c75014b9f1cc468346bb10b
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-07-13 12:40:35 +0200

    Hook up action creator through reduxForm

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostNew extends Component {
 	render() {
-		const { fields : { title, categories, content }, handleSubmit } = this.props;
+		const { fields: { title, categories, content }, handleSubmit } = this.props;
 		return (
-			<form onSubmit={handleSubmit}>
+			<form onSubmit={handleSubmit(this.props.createPost)}>
 				<h4>Create a post</h4>
 				<div className="form-group">
 					<label>Title</label>
@@ -29,7 +30,10 @@ class PostNew extends Component {
 	}
 }
 
+//Connect: first arg is mapStateToProps, second is mapDispatchToProps
+// ReduxForm: first arg is form, second mapStateToProps, third mapDisPatchToProps.
+
 export default reduxForm({
 	form: 'PostNewForm',
 	fields: ['title', 'categories', 'content']
-})(PostNew);
+}, null, { createPost })(PostNew);
