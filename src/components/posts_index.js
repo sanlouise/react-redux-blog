0a4bc8c4e80ab40235968955b277e0bee15f3ea1
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-07-13 14:33:51 +0200

    Render blog posts on index page

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -7,8 +7,20 @@ export default class PostsIndex extends Component {
 
 	componentWillMount() {
 		this.props.fetchPosts();
-		console.log('Should call action creator FETCH_POSTS')
 	}
+
+	renderPosts() {
+		return this.props.posts.map((post) => {
+			return (
+			<li className="list-group-item" key={post.id}>
+				<span className="pull-xs-right">{post.categories}</span>
+				<strong>{post.title}</strong>
+			</li>
+			);
+		});
+	}
+
+
 	render() {
 		return (
 			<div>
@@ -17,11 +29,18 @@ export default class PostsIndex extends Component {
 						Create Posts
 					</Link>
 				</div>
-				All blog posts
+				<h3>Posts</h3>
+				<ul className="list-group">
+					{this.renderPosts()}
+				</ul>
 			</div>
 
 		);
 	}
 }
 
-export default connect(null, { fetchPosts })(PostsIndex)
+function mapStateToProps(state) {
+	return {posts: state.posts.all };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex)
