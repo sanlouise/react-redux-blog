3c3f763df6076df1a3b1abcbfbee0ea4683171c8
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-07-13 15:07:59 +0200

    Add layout and route for post_show

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,13 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts_index';
 import PostNew from './components/post_new';
+import PostShow from './components/post_show'
 
 export default (
 	<Route path="/" component={App}>
 		<IndexRoute component={PostsIndex} />
 		<Route path="posts/new" component={PostNew} />
+		<Route path="posts/:id" component={PostShow} />
+
 	</Route>
 );
