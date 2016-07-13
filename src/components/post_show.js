bd094b02f61351095f220b0ce9eb5d0e3e807b67
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-07-13 16:12:31 +0200

    Enable immediate redirecting after deleting post

--- src/components/post_show.js
+++ src/components/post_show.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions/index';
+import { fetchPost, deletePost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostShow extends Component {
+
+	static contextTypes = {
+		router: PropTypes.object
+	};
+
 	componentWillMount() {
 		this.props.fetchPost(this.props.params.id);
+	}
+
+	onDeleteClick() {
+		this.props.deletePost(this.props.params.id)
+			.then(() => { 
+				this.context.router.push('/');
+			});
+	
 	}
 
 	render() {
@@ -15,13 +29,19 @@ class PostShow extends Component {
 			return <div>Loading..</div>
 		}
 
-		return (
-			<div>
-				<h3>Title: {post.title}</h3>
-				<h6>Categories: {post.categories}</h6>
-				<p>{post.content}</p>
-			</div>
-		);
+	return (
+      <div>
+        <Link to="/">Back To Home</Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}>
+          Delete Post
+        </button>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
+      </div>
+    );
 	}
 }
 
@@ -29,4 +49,4 @@ function mapStateToProps(state) {
 	return { post: state.posts.post };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostShow);
