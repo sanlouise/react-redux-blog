8937752d73f59fa16b1e45da13432c6174c4cb66
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-07-13 15:41:45 +0200

    Update post show page

--- src/components/post_show.js
+++ src/components/post_show.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions/index';
 
 class PostShow extends Component {
+	componentWillMount() {
+		this.props.fetchPost(this.props.params.id);
+	}
+
 	render() {
-		return <div>Show post {this.props.params.id}</div>;
+
+		const post = this.props.post;
+
+		if (!this.props.post) {
+			return <div>Loading..</div>
+		}
+
+		return (
+			<div>
+				<h3>Title: {post.title}</h3>
+				<h6>Categories: {post.categories}</h6>
+				<p>{post.content}</p>
+			</div>
+		);
 	}
 }
 
-export default PostShow;
+function mapStateToProps(state) {
+	return { post: state.posts.post };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostShow);
